6b6e886564241af161604891a27d63beae70c9d5
pkindalov <panayotkindalov@gmail.com>
2016-12-02 17:20:09 +0200

    Added events to navigation links

--- WebAds/src/Views/HomeView.js
+++ WebAds/src/Views/HomeView.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';;
+import React, { Component } from 'react';
+import '../Components/site.css';
 
 export default class HomeView extends Component {
     render() {
         return (
-            <div className="home-view">
-                <h1>Home</h1>
-                { this.props.username ?
-                    <p>Welcome, {this.props.username}.</p> :
-                    <p>Welcome to the WebAds.</p>
-                }
-            </div>
+
+                <div className="container">
+                    <h1 id="greetingsHeading">Greetings</h1>
+                    { this.props.username ?
+                        <p>Welcome, {this.props.username}.</p> :
+                        <p>Welcome to the WebAds.</p>
+
+                    }
+
+
+                    <p>Here you can post for <strong><u>free</u></strong> everything that you want to sell/buy. Just login into your account and
+                        click the <strong>New Advertisment</strong> link. If you are not in our system, you can still browse the collection
+                        of advertisments our other users have. Enjoy your stay!</p><br />
+
+                    <p>Get started advertising your products.
+                        There are legions of users who might love your themes — but first they need to know about it.
+                        Promote your ads on our site. Reach new audiences.
+                        It's fast and easy to get started. Select the product you want to advertise and start your campaign!
+                    </p>
+
+
+
+
+                </div>
+
+
+
+
+            // <div className="home-view">
+            //     <h1>Home</h1>
+            //     { this.props.username ?
+            //         <p>Welcome, {this.props.username}.</p> :
+            //         <p>Welcome to the WebAds.</p>
+            //     }
+            // </div>
         );
     }
 }
