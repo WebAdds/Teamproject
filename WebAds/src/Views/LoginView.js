1c33c141ab1d82b0aa732cb40d80c88e4ea1939e
pkindalov <panayotkindalov@gmail.com>
2016-12-03 16:04:29 +0200

    Added some new divs with classes tab-content, container, form-group, col-md-10.Also added button register

--- WebAds/src/Views/LoginView.js
+++ WebAds/src/Views/LoginView.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
+import '../App';
+import '../Components/NavigationBar';
+import '../Views/RegisterView';
 
 export default class LoginView extends Component {
     render() {
         return (
-            <form className="login-form" onSubmit={this.submitForm.bind(this)}>
-                <h1>Login</h1>
-                <label>
-                    <div>Username:</div>
-                    <input type="text" name="username" required
-                           ref={e => this.usernameField = e} />
-                </label>
-                <label>
-                    <div>Password:</div>
-                    <input type="password" name="password" required
-                           ref={e => this.passwordField = e} />
-                </label>
-                <div>
-                    <input type="submit" value="Login" />
+            <div className="tab-content">
+                <div className="container">
+                    <div className="form-group">
+                        <div className="col-md-10">
+                            <form className="login-form" onSubmit={this.submitForm.bind(this)}>
+                                <h1>Login</h1>
+                                <label>
+                                    <div><h5>Username:</h5></div>
+                                    <input className="form-control" type="text" name="username" required
+                                           ref={e => this.usernameField = e}/>
+                                </label>
+                                <label>
+                                    <div><h5>Password:</h5></div>
+                                    <input className="form-control" type="password" name="password" required
+                                           ref={e => this.passwordField = e}/>
+                                </label>
+                                <div className="form-group">
+                                    <input type="submit" value="Login"/>
+                                    <input type="button" value="Register" onClick={function () {
+                                        //alert('Fucking stupid react');
+
+                                    }}/>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </form>
+            </div>
         );
     }
 
@@ -26,5 +42,6 @@ export default class LoginView extends Component {
         event.preventDefault();
         this.props.onsubmit(
             this.usernameField.value, this.passwordField.value);
+
     }
 }
